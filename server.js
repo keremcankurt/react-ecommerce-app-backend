80900b12186b899323e0356841b759b661e10371
keremcankurt <127202679+keremcankurt@users.noreply.github.com>
2023-06-10 15:04:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDatabase();
 const app = express();
 app.use(express.static('public'));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://kckticaret.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
